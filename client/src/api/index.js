688a4d4d9066689f621d6d4a21dbb94e1c7c036e
HowWhat <charlau30@gmail.com>
2022-05-28 11:18:52 +0200

    fix: api access client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = `http://localhost:3001/api`
+const baseUrl = `/api`
 
 export const fetchUsers = () => axios.get(`${baseUrl}/users/`)
 export const fetchUser = (id) => axios.get(`${baseUrl}/users/${id}`)
